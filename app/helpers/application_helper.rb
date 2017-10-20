c6b8c839807a75a51e26be95c7ea29eb534b0487
João Fernandes <ojoaofernandes@gmail.com>
2017-10-19 22:47:01 -0200

    Add navbar to the management area

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def nav_link description, path
+    active = current_page?(path) ? 'active' : ''
+
+    content_tag :li, class: active do
+      link_to description, path
+    end
+  end
+
 end
